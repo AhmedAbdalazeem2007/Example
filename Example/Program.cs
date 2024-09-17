77b98a85f0a047680adcb672646ac4dda3ed7767
AhmedAbdalazeem2007 <ahmedmon997@gmail.com>
2024-09-17 14:34:29 +0300

    OtherChanges

--- Example/Program.cs
+++ Example/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(i);
             Console.WriteLine("Hello, World!");
         }
     }
